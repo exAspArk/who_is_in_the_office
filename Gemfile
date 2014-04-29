a9191576938b32070c1235dae426b6c6e74d0ff9
Evgeny Li <exaspark@gmail.com>
2014-04-29 17:32:55 +0400

    test twitter OAuth

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
 gem 'jquery-rails'
 gem 'haml'
+gem 'omniauth-twitter'
 gem 'omniauth-facebook'
 
 gem 'spring', group: :development
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, Rails.application.secrets.facebook['key'], Rails.application.secrets.facebook['secret']
 end
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :twitter, Rails.application.secrets.twitter['key'], Rails.application.secrets.twitter['secret']
+end
